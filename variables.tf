33cef8391b4c220546a2ffc17aaaf1f028a16d66
sulbhaverma <148425570+sulbhaverma@users.noreply.github.com>
2025-07-18 17:42:57 +0530

    Create variables.tf

---
+++ variables.tf
@@ -0,0 +1,11 @@
+variable "resource_group_name" {
+  default = "github-aks-rg"
+}
+
+variable "aks_cluster_name" {
+  default = "github-aks-cluster"
+}
+
+variable "location" {
+  default = "East US"
+}
